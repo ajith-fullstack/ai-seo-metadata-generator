720278ec5bbbb04887db4388cc294337bcaeec09
Ajith Kumar <ajith.p.acumen@gmail.com>
2026-05-30 14:21:47 +0530

    Fix root route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 const seoRoutes = require("./routes/seoRoutes");
 const app = express();
@@ -6,6 +7,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.use("/api", seoRoutes);
 
 app.listen(port, () => {
